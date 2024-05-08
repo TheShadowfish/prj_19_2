e274730b7581d17a8a466adaacb014a7fcd0d16e
TheShadowfish <shadowfish@yandex.ru>
2024-05-08 10:38:36 +0300

    new 2

--- main/admin.py
+++ main/admin.py
@@ -7,6 +7,6 @@ from main.models import Student
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'is_active',)
-    list_filter = ('is_active',)
+    list_display = ('first_name', 'last_name', 'is_active','avatar')
+    list_filter = ('is_active','first_name')
     search_fields = ('first_name', 'last_mame',)
